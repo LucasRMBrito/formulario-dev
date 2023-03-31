cd067e671a154baf6e64e1ffb44eaa4768ccf4fc
LucasBrito <lucasroger.mb@hotmail.com>
2023-03-30 23:33:23 -0300

    adicionando termos de uso

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 const gerarPDF = () => {
     let nome = document.querySelector("#nome")
     let cpf = document.querySelector("#cpf")
@@ -20,6 +19,53 @@ const gerarPDF = () => {
     pdf.text(`Minhas palavras : ${texto.value}`, 20 , 90)
     pdf.save('Cadastro-dev.pdf')
     
+}
+
+
+const gerarLGPD = () => {
+
+let teste = `
+TERMOS DE USO:
+
+Ao utilizar a página de cadastro, o usuário concorda com os seguintes termos:
+
+Coleta de Dados Pessoais: Ao se cadastrar nesta página, o usuário concorda 
+em fornecer informações pessoais, como nome, endereço de e-mail, endereço
+residencial, data de nascimento e número de telefone, para que possamos 
+processar sua solicitação de cadastro.
+
+Tratamento de Dados: Todos os dados fornecidos pelo usuário serão tratados 
+de acordo com a Lei Geral de Proteção de Dados (LGPD) e somente serão 
+utilizados para fins legítimos e específicos, como processamento de pedidos,
+análise de dados para melhorar nossos serviços e prevenção de fraudes.
+
+Armazenamento de Dados: Os dados pessoais fornecidos pelo usuário serão 
+armazenados em nossos servidores e somente serão acessíveis por funcionários 
+autorizados que precisam dessas informações para realizar suas funções.
+
+Compartilhamento de Dados: Não compartilhamos informações pessoais dos 
+usuários com terceiros, exceto quando necessário para cumprir uma 
+obrigação legal ou regulatória, ou quando o usuário nos conceder 
+autorização expressa para fazê-lo.
+
+Direitos do Titular dos Dados: O usuário tem o direito de acessar seus 
+dados pessoais, corrigi-los, apagá-los ou limitar seu processamento,
+bem como o direito de se opor ao processamento de seus dados. 
+Para exercer esses direitos, o usuário deve entrar em contato 
+conosco através dos meios de contato disponíveis na página.
+
+Consentimento: Ao clicar no botão de cadastro, o usuário confirma que 
+leu e concorda com estes termos de uso e com nossa Política de Privacidade.
+
+Alterações nos Termos de Uso: Podemos alterar estes termos de uso a 
+qualquer momento, mediante aviso prévio na página de cadastro ou por meio
+de comunicação direta com o usuário. É responsabilidade do usuário verificar
+regularmente esta página para tomar conhecimento de eventuais alterações.
+`
+
+let lei = new jsPDF()
+lei.text(teste, 5 , 10)
+lei.save('TERMO_DE_USO.pdf')
 }
 
 const validarCpf = (cpf=0) => {
@@ -77,5 +123,14 @@ const inserirCPF = () => {
     let variavel = document.querySelector('#cpf')
     if( variavel.value != ''){
         validarCpf(variavel.value);
+    }
+}
+
+const checando = () => {
+    let check = document.querySelector(".check")
+    if (check.checked == true) {
+        inserirCPF()
+    } else{
+        alert('É preciso aceitar os termos para se cadastrar!')
     }
 }
